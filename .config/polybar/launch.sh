9ea8d5c3bcad1640ffe3d8036480e39a60b5ea83
Socks Candy <socksthefennec@gmail.com>
2020-08-06 21:58:53 +1000

    begin adding desktop configuration

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep .1; done
+
+export DEFAULT_TITLE=$(whoami)@$(hostname)
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+case $(hostname) in
+	socks-laptop)
+		export MONITOR=eDP-1
+		polybar primary-top &
+		polybar primary-bottom &
+		;;
+	socks-desktop)
+		export MONITOR=HDMI-0
+		polybar primary-top &
+		polybar primary-bottom &
+		export MONITOR=DP-0
+		polybar secondary-top &
+		polybar secondary-bottom &
+		;;
+	*)
+		polybar primary-top &
+		polybar primary-bottom &
+		;;
+esac
+
+echo "Polybar launched..."
