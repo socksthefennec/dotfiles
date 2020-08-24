47a8389e9b39dc22ff255f2b6e7442c39602d40d
Socks Candy <socksthefennec@gmail.com>
2020-08-25 05:07:53 +1000

    swap calls to hostname with hostnamectl --static

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep .1; done
 
-export DEFAULT_TITLE=$(whoami)@$(hostname)
+export DEFAULT_TITLE=$(whoami)@$(hostnamectl --static)
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 case $(hostname) in
